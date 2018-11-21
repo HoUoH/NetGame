b91092fbc5871f32910be2e720484dd3953f609d
차은상 <jyanny14@gmail.com>
2018-11-21 14:56:31 +0900

    클라이언트 서버 연결 가능하게 만듬, 이제 while 문 안에 데이터 보낼 거 집어 넣으면 됌 텍스쳐 그리는 곳도 스레드라 데이터 보낼 때 생각이 필요

---
+++ Project_Client/Project_Client/Project_Client.cpp
@@ -0,0 +1,365 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+#include "stdafx.h"
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+#include <fstream>
+#include "Dependencies\glew.h"
+#include "Dependencies\freeglut.h"
+#include "ScnMgr.h"
+#include "resource.h"
+
+#define SERVERIP   "127.0.0.1"
+#define SERVERPORT 9000
+#define BUFSIZE    512
+
+// 대화상자 프로시저
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+// 편집 컨트롤 출력 함수
+void DisplayText(char *fmt, ...);
+// 오류 출력 함수
+void err_quit(char *msg);
+void err_display(char *msg);
+// 사용자 정의 데이터 수신 함수
+int recvn(SOCKET s, char *buf, int len, int flags);
+// 소켓 통신 스레드 함수
+DWORD WINAPI ClientMain(LPVOID arg);
+
+SOCKET sock; // 소켓
+char addrBuf[BUFSIZE + 1]; // 서버 IP 주소
+char fileBuf[BUFSIZE + 1]; // 서버에서 받을 파일 이름
+HANDLE hReadEvent, hWriteEvent; // 이벤트
+HWND hSendButton; // 보내기 버튼
+HWND hEdit1, hEdit2; // 편집 컨트롤
+
+ScnMgr *g_ScnMgr = NULL;
+DWORD prev_render_time = 0;
+BOOL W_KeyIsDown = false;
+BOOL A_KeyIsDown = false;
+BOOL S_KeyIsDown = false;
+BOOL D_KeyIsDown = false;
+
+// 대화상자 프로시저
+BOOL CALLBACK DlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+// 편집 컨트롤 출력 함수
+void DisplayText(char *fmt, ...);
+
+// 소켓 함수 오류 출력 후 종료
+void err_quit(char *msg);
+
+// 소켓 함수 오류 출력
+void err_display(char *msg);
+
+// 사용자 정의 데이터 수신 함수
+int recvn(SOCKET s, char *buf, int len, int flags);
+
+// TCP 클라이언트 시작 부분
+DWORD WINAPI ClientMain(LPVOID arg);
+
+//매 프레임 출력 함수
+void RenderScene(void);
+
+void Idle(void);
+
+//키 input 관련
+void MouseInput(int button, int state, int x, int y);
+
+void KeyDownInput(unsigned char key, int x, int y);
+
+void KeyUpInput(unsigned char key, int x, int y);
+
+void SpecialKeyInput(int key, int x, int y);
+
+DWORD WINAPI DrawMain(LPVOID arg);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+
+	hReadEvent = CreateEvent(NULL, FALSE, TRUE, NULL);
+	if (hReadEvent == NULL) return 1;
+	hWriteEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+	if (hWriteEvent == NULL) return 1;
+
+	CreateThread(NULL, 0, ClientMain, NULL, 0, NULL);
+
+	DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
+
+	CloseHandle(hReadEvent);
+	CloseHandle(hWriteEvent);
+
+	return 0;
+}
+
+// 대화상자 프로시저
+BOOL CALLBACK DlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg) {
+	case WM_INITDIALOG:
+		hEdit1 = GetDlgItem(hDlg, IDC_EDIT1);
+		hEdit2 = GetDlgItem(hDlg, IDC_EDIT2);
+		hSendButton = GetDlgItem(hDlg, IDOK);
+		SendMessage(hEdit1, EM_SETLIMITTEXT, BUFSIZE, 0);
+		return TRUE;
+	case WM_COMMAND:
+		switch (LOWORD(wParam)) {
+		case IDOK:
+			SetDlgItemText(hDlg, IDC_EDIT2, "");
+			EnableWindow(hSendButton, FALSE); // 보내기 버튼 비활성화
+			WaitForSingleObject(hReadEvent, INFINITE); // 이벤트 기다리기
+			GetDlgItemText(hDlg, IDC_EDIT1, addrBuf, BUFSIZE + 1);
+			DisplayText(addrBuf);
+			SetEvent(hWriteEvent); // 쓰기 완료 알리기
+			return TRUE;
+		case IDCANCEL:
+			EndDialog(hDlg, IDCANCEL);
+			return TRUE;
+		}
+		return FALSE;
+	}
+	return FALSE;
+}
+
+// 편집 컨트롤 출력 함수
+void DisplayText(char *fmt, ...)
+{
+	va_list arg;
+	va_start(arg, fmt);
+
+	char cbuf[BUFSIZE + 256];
+	vsprintf(cbuf, fmt, arg);
+
+	int nLength = GetWindowTextLength(hEdit2);
+	SendMessage(hEdit2, EM_SETSEL, nLength, nLength);
+	SendMessage(hEdit2, EM_REPLACESEL, FALSE, (LPARAM)cbuf);
+
+	va_end(arg);
+}
+
+// 소켓 함수 오류 출력 후 종료0
+void err_quit(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+// 소켓 함수 오류 출력
+void err_display(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	DisplayText((char*)"[%s] %s", msg, (char *)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+// 사용자 정의 데이터 수신 함수
+int recvn(SOCKET s, char *buf, int len, int flags)
+{
+	int received;
+	char *ptr = buf;
+	int left = len;
+
+	while (left > 0) {
+		received = recv(s, ptr, left, flags);
+		if (received == SOCKET_ERROR)
+			return SOCKET_ERROR;
+		else if (received == 0)
+			break;
+		left -= received;
+		ptr += received;
+	}
+
+	return (len - left);
+}
+
+// TCP 클라이언트 시작 부분
+DWORD WINAPI ClientMain(LPVOID arg)
+{
+	//계속 입력을 받아야 하기 때문에 while안에 넣는다
+	while (1) {
+		//클릭 기다리기
+		WaitForSingleObject(hWriteEvent, INFINITE);
+
+		int retval;
+		int len = 0;
+		char buf[BUFSIZE] = { NULL };
+
+		// 윈속 초기화
+		WSADATA wsa;
+		if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+			return 1;
+
+		// socket()
+		sock = socket(AF_INET, SOCK_STREAM, 0);
+		if (sock == INVALID_SOCKET) err_quit((char*)"socket()");
+
+		// connect()
+		SOCKADDR_IN serveraddr;
+		ZeroMemory(&serveraddr, sizeof(serveraddr));
+		serveraddr.sin_family = AF_INET;
+		serveraddr.sin_addr.s_addr = inet_addr(addrBuf);
+		serveraddr.sin_port = htons(SERVERPORT);
+		retval = connect(sock, (SOCKADDR *)&serveraddr, sizeof(serveraddr));
+		if (retval == SOCKET_ERROR) err_quit((char*)"connect()");
+		CreateThread(NULL, 0, DrawMain, NULL, 0, NULL);
+
+		// 서버와 데이터 통신
+		// 홀아 데이터를 넣어라!
+		while (1) {
+		}
+
+		EnableWindow(hSendButton, TRUE); // 보내기 버튼 활성화
+		SetEvent(hReadEvent); // 읽기 완료 알리기
+
+
+		// closesocket()
+		closesocket(sock);
+
+		// 윈속 종료
+		WSACleanup();
+		DisplayText((char*)"데이터 받기 끝!\r\n");
+	}
+
+	return 0;
+}
+
+void RenderScene(void)	//1초에 최소 60번 이상 출력되어야 하는 함수
+{
+	if (prev_render_time == 0)
+		prev_render_time = timeGetTime();
+	//Elapsed Time
+	DWORD current_time = timeGetTime();
+	DWORD elapsed_time = current_time - prev_render_time;
+	prev_render_time = current_time;
+	float eTime = elapsed_time / 1000.f;//convert to second
+//	std::cout << "elapsedTime:" << eTime << std::endl;
+	//std::cout << "w:" << W_KeyIsDown << " a:" << A_KeyIsDown << " s:" << S_KeyIsDown << " d:" << D_KeyIsDown << std::endl;
+
+	float forceX = 0.f, forceY = 0.f;
+	float amount = 3000.0f;		// 1N = 1000 기준
+
+	if (W_KeyIsDown)
+		forceY += amount;
+	if (A_KeyIsDown)
+		forceX -= amount;
+	if (S_KeyIsDown)
+		forceY -= amount;
+	if (D_KeyIsDown)
+		forceX += amount;
+
+	g_ScnMgr->ApplyForce(forceX, forceY, eTime);
+	g_ScnMgr->BreakMovement(W_KeyIsDown, S_KeyIsDown, D_KeyIsDown, A_KeyIsDown, eTime);
+	g_ScnMgr->Update(eTime);
+
+	// 클라입장
+	// send() 하고 / 서버가 계산한 최종 위치 recv()
+
+	g_ScnMgr->RenderScene();
+	glutSwapBuffers();
+}
+
+void Idle(void)
+{
+	RenderScene();
+}
+
+void MouseInput(int button, int state, int x, int y)
+{
+	RenderScene();
+}
+
+void KeyDownInput(unsigned char key, int x, int y)
+{
+	float amount = 0.1f;
+	switch (key) {
+	case 'w':
+		W_KeyIsDown = TRUE;
+		break;
+	case 'a':
+		A_KeyIsDown = TRUE;
+		break;
+	case 's':
+		S_KeyIsDown = TRUE;
+		break;
+	case 'd':
+		D_KeyIsDown = TRUE;
+		break;
+	default:
+		break;
+	}
+}
+
+void KeyUpInput(unsigned char key, int x, int y) {
+	switch (key) {
+	case 'w':
+		W_KeyIsDown = FALSE;
+		break;
+	case 'a':
+		A_KeyIsDown = FALSE;
+		break;
+	case 's':
+		S_KeyIsDown = FALSE;
+		break;
+	case 'd':
+		D_KeyIsDown = FALSE;
+		break;
+	default:
+
+		break;
+	}
+	RenderScene();
+}
+
+void SpecialKeyInput(int key, int x, int y)
+{
+	//시작 키를 눌렀을 경우 (F1키)
+	g_ScnMgr->joinClick(key);
+	RenderScene();
+}
+
+DWORD WINAPI DrawMain(LPVOID arg) {
+	
+	int argc = 1;
+	char* argv[1] = {(char*)"Project_Client.exe"};	
+	glutInit(&argc, argv);
+	
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(WINDOW_SIZEX, WINDOW_SIZEY);
+	glutCreateWindow("KPU");
+
+	glewInit();
+
+	glutDisplayFunc(RenderScene);
+	glutIdleFunc(Idle);
+	glutKeyboardFunc(KeyDownInput);
+	glutKeyboardUpFunc(KeyUpInput);
+	glutSpecialFunc(SpecialKeyInput);
+	glutSetKeyRepeat(GLUT_KEY_REPEAT_OFF);
+	glutMouseFunc(MouseInput);
+
+	g_ScnMgr = new ScnMgr();
+	
+	while (1)
+	{
+		glutMainLoop();
+	}
+	delete g_ScnMgr;
+
+	return 0;
+}
---
+++ Project_Server/Collision-DESKTOP-HUTGLNB.cpp
@@ -0,0 +1,219 @@
+#include "pch.h"
+#include "Collision.h"
+#include "Server_Global.h"
+//#include "object.h"
+
+float e = 1.f;	// Åº¼º °è¼ö
+
+/*
+bool CollisionCheck(object * oA, object * oB)
+{
+
+	float oA_rad, oB_rad;
+	float sizeX, sizeY;
+	float x1, y1;
+	float x2, y2;
+	oA->GetSize(&oA_rad, &sizeX);
+	oB->GetSize(&oB_rad, &sizeY);
+	oA->GetLocation(&x1, &y1);
+	oB->GetLocation(&x2, &y2);
+
+
+	float colLenLimit = oA_rad / 2 + oB_rad / 2;
+	float distBtwPoints = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+
+
+	if (colLenLimit <= distBtwPoints)
+		return false;
+
+	return true;
+}
+*/
+
+/*
+bool CollisionCheck(object *oA, float posx, float posy)
+{
+
+	float oA_rad;
+	float Secondsize;
+	float x1, y1;
+	float x2, y2;
+	oA->GetSize(&oA_rad, &Secondsize);
+	oA->GetLocation(&x1, &y1);
+
+
+	float colLenLimit = oA_rad / 2 + PLAYER_SIZE / 2;
+	float distBtwPoints = sqrt((posx - x1)*(posx - x1) + (posy - y1)*(posy - y1));
+
+
+	if (colLenLimit <= distBtwPoints) {
+		//printf("¾È°ãÃÄ\n");
+		return false;
+	}
+
+	//printf("°ãÃÄ\n");
+	return true;
+}
+*/
+
+//object vs postion ver
+bool CollisionCheck(const float& oA_rad, const float& oA_x, const float& oA_y, float posx, float posy)
+{
+
+	float colLenLimit = oA_rad / 2 + PLAYER_SIZE / 2;
+	float distBtwPoints = sqrt((posx - oA_x)*(posx - oA_x) + (posy - oA_y)*(posy - oA_y));
+
+
+	if (colLenLimit <= distBtwPoints) {
+		//printf("¾È°ãÃÄ\n");
+		return false;
+	}
+
+	//printf("°ãÃÄ\n");
+	return true;
+}
+
+//object vs object ver
+bool CollisionCheck(const float& oA_rad, const float& oA_x, const float& oA_y, 
+	const float& oB_rad, const float& oB_x, const float& oB_y)
+{
+
+	float colLenLimit = oA_rad / 2 + oB_rad / 2;
+	float distBtwPoints = sqrt((oA_x - oB_x)*(oA_x - oB_x) + (oA_y - oB_y)*(oA_y - oB_y));
+
+
+	if (colLenLimit <= distBtwPoints)
+		return false;
+
+	return true;
+}
+
+/*
+ÀÌ ºÎºÐÀº ScnMgr·Î ³Ñ¾î°¨
+void CollisionReaction(class object* oA, class object* oB)
+{
+	int oA_Kind, oB_Kind = 0;
+	oA->GetKind(&oA_Kind);
+	oB->GetKind(&oB_Kind);
+
+	float oA_X, oA_Y = 0.f;
+	float oB_X, oB_Y = 0.f;
+	oA->GetLocation(&oA_X, &oA_Y);
+	oB->GetLocation(&oB_X, &oB_Y);
+
+	float oA_rad, oB_rad, s1, s2 = 0.f;
+	oA->GetSize(&oA_rad, &s1);
+	oB->GetSize(&oB_rad, &s2);
+
+	float oA_VX, oA_VY = 0.f;
+	oA->GetVelocity(&oA_VX, &oA_VY);
+
+	float oB_VX, oB_VY = 0.f;
+	oB->GetVelocity(&oB_VX, &oB_VY);
+
+	float oA_M, oB_M = 0.f;
+	oA->GetMass(&oA_M);
+	oB->GetMass(&oB_M);
+
+	if (oA_Kind == oB_Kind)
+	{
+		float dx = oA_X - oB_X;
+		float dy = oA_Y - oB_Y;
+		float dab = fabsf(sqrt(dx*dx + dy * dy));
+
+		float sinTheta = dy / fabsf(sqrt(dx*dx + dy * dy));
+		float cosTheta = dx / fabsf(sqrt(dx*dx + dy * dy));
+
+		float vxAp = (oA_M - e * oB_M) / (oA_M + oB_M)*(oA_VX*cosTheta + oA_VY * sinTheta) +
+			(oB_M + e * oB_M) / (oA_M + oB_M)*(oB_VX*cosTheta + oB_VY * sinTheta);
+		float vxBp = (oA_M + e * oA_M) / (oA_M + oB_M)*(oA_VX*cosTheta + oA_VY * sinTheta) +
+			(oB_M - e * oA_M) / (oA_M + oB_M)*(oB_VX*cosTheta + oB_VY * sinTheta);
+
+		float vyAp = oA_VX * (-sinTheta) + oA_VY * cosTheta;
+		float vyBp = oB_VX * (-sinTheta) + oB_VY * cosTheta;
+
+		oA->SetVelocity(vxAp, vyAp);
+		oB->SetVelocity(vyAp, vyBp);
+
+	}
+	else
+	{
+		if (oA_Kind == KIND_HERO)
+		{
+			float oB_VelMag = sqrt(oB_VX*oB_VX + oB_VY * oB_VY);
+			if (oB_VelMag > FLT_EPSILON)
+				oA->SetIsVisible(false);
+		}
+		else if (oB_Kind == KIND_HERO) {
+			float oA_VelMag = sqrt(oA_VX*oA_VX + oA_VY * oA_VY);
+			if (oA_VelMag > FLT_EPSILON)
+				oB->SetIsVisible(false);
+		}
+	}
+
+
+}
+*/
+
+/*
+void WallCollision(class object* obj)
+{
+	float posX, posY = 0;
+	float rad, height = 0;
+	obj->GetLocation(&posX, &posY);
+	obj->GetSize(&rad, &height);
+
+	if (posX - rad / 2 <= -WINDOW_SIZEX / 2 || posX + rad / 2 >= WINDOW_SIZEX / 2) {
+		float vx, vy;
+		obj->GetPreLocation(&posX, &posY);
+		obj->SetLocation(posX, posY);
+		obj->GetVelocity(&vx, &vy);
+		obj->SetVelocity(-vx * 2.f, vy);
+	}
+	if (posY - rad / 2 <= -WINDOW_SIZEY / 2 || posY + rad / 2 >= WINDOW_SIZEY / 2) {
+		float vx, vy;
+		obj->GetPreLocation(&posX, &posY);
+		obj->SetLocation(posX, posY);
+		obj->GetVelocity(&vx, &vy);
+		obj->SetVelocity(vx, -vy * 2.f);
+	}
+
+
+}
+*/
+
+char WallCollision(const float& posX, const float& posY, const float& rad, const float& height)
+{
+	if (posX - rad / 2 <= -WINDOW_SIZEX / 2 || posX + rad / 2 >= WINDOW_SIZEX / 2) {
+		//Right or Left
+		return 'r';
+	}
+	if (posY - rad / 2 <= -WINDOW_SIZEY / 2 || posY + rad / 2 >= WINDOW_SIZEY / 2) {
+		//Up or Down
+		return 'u';
+	}
+
+	//return none
+	return 'n';
+}
+
+/*
+bool JoinCollision(class object* obj, float posx, float posy) {
+	for (int i = 0; i < MAX_OBJECTS; ++i) {
+		if (obj[i].GetIsVisible() == true) {
+			if (CollisionCheck(&obj[i], posx, posy))
+				return true;
+		}
+	}
+
+	//printf("¾È°ãÃÄ\n");
+	return false;
+}
+*/
+
+bool JoinCollision(const float& obj_Rad ,const float& obj_PosX, const float& obj_PosY, const float& posx, const float& posy) {
+	if (CollisionCheck(obj_Rad, obj_PosX, obj_PosY, posx, posy))
+		return true;
+	//printf("¾È°ãÃÄ\n");
+	return false;
+}
